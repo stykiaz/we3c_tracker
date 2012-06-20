5e4528460244c747022ec4e33a3699b33a182569
styki <styki@styki>
2012-06-20 17:57:00 +0100

    updated start proc

--- start_prod.sh
+++ start_prod.sh
@@ -14,4 +14,13 @@ rm -fr target
 mv -f we3c-tracker/* ./
 rm -fr we3c-tracker
 /opt/play-2.0.1/play stop
+/opt/Play20/play stop
+
+if [ -f RUNNING_PID ]
+        then
+                runningpid=$(cat RUNNING_PID)
+                rm RUNNING_PID
+                kill $runningpid
+fi
+
 nohup /www/sites/we3c/tracker/target/start -Dhttp.port=9001 -Dconfig.resource=prod.conf &
