a32ec8456cb11c09d45d7f33a71da296b26c68e6
Stanislav <stanislav.pains@gmail.com>
2013-02-16 14:36:52 -0800

    Updated app/controllers/AdminSecurity.java

--- app/controllers/AdminSecurity.java
+++ app/controllers/AdminSecurity.java
@@ -10,11 +10,13 @@ public class AdminSecurity extends Security.Authenticator {
 	@Override
     public String getUsername(Context ctx) {
         return ctx.session().get("admin_username");
+      
     }
     
     @Override
     public Result onUnauthorized(Context ctx) {
         return redirect(controllers.routes.Application.login());
+      
     }
 	
 }
